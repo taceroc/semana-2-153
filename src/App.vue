ca2b5ee872c865680beb89104cb6ecef68319e6c
taceroc <taceroc@unal.edu.co>
2020-12-05 19:09:51 -0500

    seccion de integrantes con componente e informacion en data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,62 @@
 <template>
   <div id="app">
+    <div class="container-fluid">
+      <div class="row justify-content-center mb-3 mt-0">
+        <div
+          class="col-sm-6 col-xs-12 col-lg-3 mt-5"
+          v-for="(item, index) of team"
+          v-bind:key="index"
+        >
+          <team-card v-bind:member="item"></team-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import TeamCard from "./components/TeamCard.vue";
 
 export default {
-  name: 'App',
-}
+  name: "App",
+  components: { TeamCard },
+  data() {
+    return {
+      team: [
+        {
+          codigo: 1,
+          nombre: "Tatiana Acero Cuellar",
+          descripcion:
+            "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+          rol: "Desarrollador backend",
+          image:require('./assets/Tatiana.png')
+        },
+        {
+          codigo: 2,
+          nombre: "Cristian Camilo Parra Parra",
+          descripcion:
+            "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+          rol: "Desarrollador backend",
+          image: require('./assets/Camilo.png'),
+        },
+        {
+          codigo: 3,
+          nombre: "Daniel Picon Agudelo",
+          descripcion:
+            "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+          rol: "Desarrollador backend",
+          image: require('./assets/Daniel.png'),
+        },
+        {
+          codigo: 4,
+          nombre: "Hanssel Neira",
+          descripcion:
+            "Lorem ipsum dolor sit amet, consectetur adipiscingelit. Mauris condimentum ac elit et accumsan.",
+          rol: "Desarrollador backend",
+          image: "path/to/image",
+        },
+      ],
+    };
+  },
+};
 </script>
-
